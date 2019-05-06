d11f104f4d021006e4b84a5db7b35f2f00ebca57
Foldcc <1813547935@qq.com>
2019-05-07 00:14:55 +0800

    增加Color动画组件的自定义面板

---
+++ Assets/MintAnimation/Scripts/Components/MintAnimation_Color.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MintAnimation
+{
+    public class MintAnimation_Color : MonoBehaviour
+    {
+        public MintAnimationInfo    AnimationInfo;
+        public bool                 IsAutoPlay = true;
+    }
+}
