ac5baba97fd1e8e8e169313b17439c6399869032
haourince <haourince@outlook.com>
2019-05-07 18:52:11 +0800

    完善color动画组件

--- Assets/MintAnimation/Scripts/Components/MintAnimation_Color.cs
+++ Assets/MintAnimation/Scripts/Components/MintAnimation_Color.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace MintAnimation
 {
@@ -8,5 +9,54 @@ namespace MintAnimation
     {
         public MintAnimationInfo    AnimationInfo;
         public bool                 IsAutoPlay = true;
+
+        private Color               mGetColor;
+        private Graphic             mGrahic;
+        private Material            mMaterail;
+
+        private MintAnimationClip<Color> mMintAnimationClip;
+
+        private void OnEnable()
+        {
+            init();
+        }
+
+        private void OnDisable()
+        {
+            mMintAnimationClip?.Stop();
+        }
+
+        private void init()
+        {
+            this.mGrahic = this.gameObject.GetComponent<Graphic>();
+            var m = this.gameObject.GetComponent<MeshRenderer>();
+            if (m != null) this.mMaterail = m.material;
+
+            AnimationInfo.AutoStartValue = false;
+            mMintAnimationClip = new MintAnimationClip<Color>(getter , setter , AnimationInfo);
+            if (IsAutoPlay)
+            {
+                mMintAnimationClip.Play();
+            }
+        }
+
+        private Color getter()
+        {
+            return mGetColor;
+        }
+
+        private void setter(Color rColor)
+        {
+            this.mGetColor = rColor;
+
+            if (this.mGrahic != null)
+            {
+                this.mGrahic.color = rColor;
+            }
+            if (this.mMaterail != null)
+            {
+                this.mMaterail.color = rColor;
+            }
+        }
     }
 }
