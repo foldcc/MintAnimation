7b7f6feff9daf0c95d0b52cccaa5a7d18e911896
Foldcc <1813547935@qq.com>
2019-05-14 00:53:22 +0800

    动画组件 base改为泛型扩展，更加灵活，现在使用stop函数会使动画播放结束（系数=1）并且停止，将init getter setter作为模板，可扩展

--- Assets/MintAnimation/Scripts/Components/MintAnimation_Base.cs
+++ Assets/MintAnimation/Scripts/Components/MintAnimation_Base.cs
@@ -4,10 +4,35 @@ using UnityEngine;
 
 namespace MintAnimation
 {
-	public abstract class MintAnimation_Base : MonoBehaviour
+	public abstract class MintAnimation_Base<T> : MonoBehaviour
 	{
-        public MintAnimationInfo AnimationInfo;
-        public bool IsAutoPlay = true;
+        public MintAnimationInfo        AnimationInfo;
+        public bool                     IsAutoPlay = true;
 
+        private bool                    _isFristInit = true;
+
+        protected MintAnimationClip<T>  mMintAnimationClip;
+
+        private void OnEnable()
+        {
+            init();
+            if (AnimationInfo.AutoStartValue && _isFristInit) AnimationInfo.SetStartValue<T>(getter());
+            _isFristInit = false;
+            if (IsAutoPlay)
+            {
+                mMintAnimationClip.Play();
+            }
+        }
+        private void OnDisable()
+        {
+            mMintAnimationClip?.Stop();
+        }
+
+        protected virtual void init() {
+            mMintAnimationClip = new MintAnimationClip<T>(getter, setter, AnimationInfo);
+        }
+
+        protected virtual T getter(){return default;}
+        protected virtual void setter(T value){ }
     }
 }
