16175cb39563a18446cbd34367a9ad969f36681a
Foldcc <1813547935@qq.com>
2019-05-19 23:44:03 +0800

    Add "Canvas Alpha" animation component

---
+++ Assets/MintAnimation/Editor/MintAnimation_EditorCanvasAlpha.cs
@@ -0,0 +1,48 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace MintAnimation.Editor {
+
+    [CustomEditor(typeof(MintAnimation.MintAnimation_CanvasAlpha), true)]
+    public class MintAnimation_EditorCanvasAlpha : MintAnimation_EditorBase
+    {
+        private SerializedProperty StartAlpha;
+        private SerializedProperty EndAlpha;
+
+        protected override void Init()
+        {
+            base.Init();
+            StartAlpha = AnimationInfo.FindPropertyRelative("StartF");
+            EndAlpha = AnimationInfo.FindPropertyRelative("EndF");
+        }
+
+
+        protected override void DrawTitle()
+        {
+            GUIStyle gUIStyle = new GUIStyle();
+            gUIStyle.fontSize = 18;
+            gUIStyle.normal.textColor = new Color32(56, 56, 56, 255);
+            gUIStyle.normal.background = MintAnimation_EditorBase.GetTexture2D(new Color32(138, 255, 0, 255));
+            gUIStyle.alignment = TextAnchor.MiddleCenter;
+            gUIStyle.margin = new RectOffset(0, 0, 8, 0);
+            GUILayout.Box(" Mint Canvas Alpha ", gUIStyle);
+            gUIStyle = null;
+        }
+
+        public override void Draw()
+        {
+            base.Draw();
+
+            EditorGUILayout.PropertyField(AutoStartValue);
+            GUILayout.Space(10);
+
+            if (!AutoStartValue.boolValue)
+            {
+                EditorGUILayout.Slider(StartAlpha, 0 , 1 , new GUIContent("StartAlpha"));
+            }
+            EditorGUILayout.Slider(EndAlpha, 0, 1, new GUIContent("EndAlpha"));
+        }
+       
+
+    }
+}
---
+++ Assets/MintAnimation/Runtime/Components/MintAnimation_CanvasAlpha.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MintAnimation {
+
+    [AddComponentMenu("MintAnimation/CanvasAlphaAnimation", 1) , RequireComponent(typeof(CanvasGroup))]
+    public class MintAnimation_CanvasAlpha : MintAnimation_Base<float>
+    {
+        private CanvasGroup mCanvasGroup;
+
+        protected override void init()
+        {
+            mCanvasGroup = GetComponent<CanvasGroup>();
+            base.init();
+        }
+
+        protected override float getter()
+        {
+            return mCanvasGroup.alpha;
+        }
+
+        protected override void setter(float value)
+        {
+            mCanvasGroup.alpha = value;
+        }
+    }
+
+}
