43152741e3112719a6970ba494eff6cc81971b90
Foldcc <1813547935@qq.com>
2019-06-09 21:01:23 +0800

    增加DataBase

---
+++ Assets/MintAnimation/Runtime/Core/Data/MintAnimationDataBase.cs
@@ -0,0 +1,24 @@
+﻿﻿using UnityEngine;
+
+namespace MintAnimation.Core
+{
+    public abstract class MintAnimationDataBase<T>
+    {
+        public MintAnimationOptions Options;
+        
+        public T StartValue;
+        public T EndValue;
+        
+        public virtual T GetProgress(float nowTime)
+        {
+            return default;
+        }
+
+        public void SetStartValue(T value)
+        {
+            StartValue = value;
+        }
+
+    }
+}
+
