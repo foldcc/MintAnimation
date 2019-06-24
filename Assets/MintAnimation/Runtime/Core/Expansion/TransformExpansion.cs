e3ad78d96d9f874e0e4eb2807d5870fe5f4f15c2
haourince <haourince@outlook.com>
2019-06-24 20:18:02 +0800

    增加transform拓展方法

---
+++ Assets/MintAnimation/Runtime/Core/Expansion/TransformExpansion.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+namespace MintAnimation.Core
+{
+	public static class TransformExpansion
+	{
+		public static MintAnimationClip<Vector3> RotationTo(this Transform transform, Vector3 endRotation , float duration , MintEaseMethod easeTypoe = MintEaseMethod.Linear)
+		{
+			MintAnimationDataBase<Vector3> animationInfo = new MintAnimationDataVector3();
+			animationInfo.StartValue = transform.localRotation.eulerAngles;
+			animationInfo.EndValue = endRotation;
+			animationInfo.Options = new MintAnimationOptions
+			{
+				EaseType = easeTypoe,
+				Duration =  duration,
+				AutoStartValue = false
+			};
+			return new MintAnimationClip<Vector3>(() => transform.localEulerAngles,
+				rValue => transform.localEulerAngles = rValue, animationInfo);
+		}
+		
+	}
+}
